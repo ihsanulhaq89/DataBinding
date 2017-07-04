9b2b86801c17c09378c3465c6be9ddbe77db8528
ihsanulhaq <ihsanulhaq89@gmail.com>
2017-07-04 05:11:25 +0500

    added image touch

--- app/src/main/java/databindingapp/example/com/databinding/ZoomActivity.java
+++ app/src/main/java/databindingapp/example/com/databinding/ZoomActivity.java
@@ -2,23 +2,34 @@ package databindingapp.example.com.databinding;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 import android.widget.SeekBar;
+
+import databindingapp.example.com.databinding.viewmodel.TouchImageView;
 
 public class ZoomActivity extends AppCompatActivity {
 
-    private ImageView img;
+    private TouchImageView imgTouch;
     private SeekBar seekbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoom);
+        imgTouch = (TouchImageView) findViewById(R.id.imgimg);
+        seekbar = (SeekBar) findViewById(R.id.seekbar);
+        seekbar.setOnSeekBarChangeListener(listener);
+    }
+
     private SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener() {
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            progress = progress-30;
+            progress = progress - 30;
+            // range of value being provide as angle of rotation are -30 < 0 < 30
             int abs_progress = Math.abs(progress);
-            img.setRotation((float) progress);
-
-            float scale = 1.0f + ((abs_progress/100.0f) * 2.0f);
-            img.setScaleX(scale);
-            img.setScaleY(scale);
+            imgTouch.setRotation((float) progress);
+            float scale = 1.0f + ((abs_progress / 100.0f) * 3.0f);
+            imgTouch.setScaleX(scale);
+            imgTouch.setScaleY(scale);
         }
 
         @Override
@@ -31,14 +42,5 @@ public class ZoomActivity extends AppCompatActivity {
 
         }
     };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_zoom);
-        img = (ImageView) findViewById(R.id.image);
-        seekbar = (SeekBar) findViewById(R.id.seekbar);
-        seekbar.setOnSeekBarChangeListener(listener);
-    }
 
 }
